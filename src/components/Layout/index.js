3e80e1a883b13874308f5716265671cd476d8f01
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2019-12-02 08:58:35 -0200

    feat: Adding use debounce

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import Header from '@/components/Header'
+import { useDebouncedCallback } from 'use-debounce'
 
 import Styled from './styled'
 import Footer from '../Footer'
@@ -8,8 +9,16 @@ import { useThemeContext } from '../../contexts/ThemeContext'
 
 const Layout = ({ children }) => {
   const { isDark, toggleDarkMode } = useThemeContext()
+  const [debouncedCallback] = useDebouncedCallback(
+    () => {
+      console.log`Handling`
+      toggleDarkMode(!isDark)
+    },
+    100
+  )
+
   return (
-    <Styled.Container>
+    <Styled.Container onWheel={debouncedCallback}>
       <Header />
       <Styled.Content>
         {children}
