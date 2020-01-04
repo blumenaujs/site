884573b87737aac85e54971f11fc54fb895ef115
Marcos <vmarcosp.pereira@gmail.com>
2020-01-04 16:12:58 -0300

    feat(BurgerButton): Adding animation

--- src/components/BurgerButton/index.js
+++ src/components/BurgerButton/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import QueueAnim from 'rc-queue-anim'
+
 import propTypes from 'prop-types'
 import Styled from './styled'
 
 const BurgerButton = ({ isOpen, onClick }) => (
-  <Styled.BurgerButton onClick={onClick} isOpen={isOpen}>
-    <div />
-  </Styled.BurgerButton>
+  <QueueAnim duration={800} type={['right']} delay={400}>
+    <Styled.BurgerButton key='1' onClick={onClick} isOpen={isOpen}>
+      <div />
+    </Styled.BurgerButton>
+  </QueueAnim>
 )
 
 BurgerButton.propTypes = {
