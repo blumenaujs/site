f79d725bb97d3007350578049edcde096690d467
Marcos <vmarcosp.pereira@gmail.com>
2020-01-04 16:15:52 -0300

    fix(JoinPage): Fix animation

--- src/components/Join/index.js
+++ src/components/Join/index.js
@@ -13,20 +13,18 @@ const Join = () => (
     <div key={1}>
       <Title>Faça parte</Title>
       <Text>O BlumenauJS está presente em diversas plataformas, venha interagir em um dos canais de comunicação!</Text>
-      <QueueAnim component={Styled.LinksContainer} duration={800} type='scale' delay={400}>
 
-        <Styled.Link key={1} href='https://chat.whatsapp.com/CaAfrllxOlC9KEkWf98qb5' target='_blank'>
-          <WhatsIcon />
-        </Styled.Link>
+      <Styled.Link href='https://chat.whatsapp.com/CaAfrllxOlC9KEkWf98qb5' target='_blank'>
+        <WhatsIcon />
+      </Styled.Link>
 
-        <Styled.Link key={2} href='https://join.slack.com/t/blumenaujs/shared_invite/enQtNTIxNDQ5MjUwODM2LWZjOTZmNWU3NWEwZDAzNGIyM2MwZDEwZjcyMjY3NmQyY2I3NjZkNDZkMzUyZWJiMzllMDBiMmJlOWQ2NjRhNTg' target='_blank'>
-          <SlackIcon />
-        </Styled.Link>
+      <Styled.Link href='https://join.slack.com/t/blumenaujs/shared_invite/enQtNTIxNDQ5MjUwODM2LWZjOTZmNWU3NWEwZDAzNGIyM2MwZDEwZjcyMjY3NmQyY2I3NjZkNDZkMzUyZWJiMzllMDBiMmJlOWQ2NjRhNTg' target='_blank'>
+        <SlackIcon />
+      </Styled.Link>
 
-        <Styled.Link key={3} href='https://discord.gg/JU9ywc' target='_blank'>
-          <DiscordIcon />
-        </Styled.Link>
-      </QueueAnim>
+      <Styled.Link href='https://discord.gg/JU9ywc' target='_blank'>
+        <DiscordIcon />
+      </Styled.Link>
     </div>
   </QueueAnim>
 )
